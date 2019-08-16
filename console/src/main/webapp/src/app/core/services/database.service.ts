f486429853d2b27efa1e0e47d1907e22b12ea5cc
CwaniX <mccwanix@gmail.com>
2019-08-16 09:57:52 +0200

    Database service mock

--- console/src/main/webapp/src/app/core/services/database.service.ts
+++ console/src/main/webapp/src/app/core/services/database.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SUNDatabaseAction } from 'src/app/shared/models/sun-database-action';
 import { Observable, of } from 'rxjs';
+import { SUNServer, SUNServerCategory } from 'src/app/shared/models/sun-server';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,19 @@ import { Observable, of } from 'rxjs';
 export class DatabaseService {
 
   constructor(private http: HttpClient) { }
+
+  public getServerData(): SUNServer {
+    const server: SUNServer = {
+      id: 0,
+      category: SUNServerCategory.DBPROXY,
+      password: "",
+      username: "test",
+      ip: "127.0.0.1",
+      port: "44405"
+    }
+
+    return server;
+  }
 
   public getActionsList(): Observable<SUNDatabaseAction[]> {
     //return this.http.get<SUNServer[]>('/api/secure/server').pipe();
